a441aa42a7c852626b6f13430feb77877ff04b14
azhida <334064034@qq.com>
2021-12-02 19:24:01 +0800

    完善 Toolï¼::loggerCustom()

--- src/Tool.php
+++ src/Tool.php
@@ -2,6 +2,9 @@
 
 namespace Azhida\Tools;
 
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
 class Tool
 {
     public static function resSuccessMsg($msg = '', $data = [], $meta = [])
@@ -82,14 +85,31 @@ class Tool
     }
 
     // 自定义日志
-    public static function loggerCustom($controller_name, $function_name, $message, $context = [], $echo_only = false) {
-        $message = $controller_name . '::' . $function_name . '() ' . $message . " => ";
+    public static function loggerCustom($controller_name, $function_name, $message, $context = [], $echo_only = false, $log_file_name = '') {
+        $echo_message = $controller_name . '::' . $function_name . '() ' . $message . " => ";
         if (!is_array($context)) {
             $context = [$context];
         }
-        if ($echo_only) return $message . json_encode($context, JSON_UNESCAPED_UNICODE) . "\n";
+        if ($echo_only) return $echo_message . json_encode($context, JSON_UNESCAPED_UNICODE) . "\n";
         $message .= "\n";
-        logger($message, $context);
+//        logger($message, $context);
+
+        $log_file = self::getLogFile($log_file_name);
+        $logger = new Logger($controller_name .'::'. $function_name);
+        $logger->pushHandler(new StreamHandler($log_file, Logger::INFO));
+        $logger->info($message, $context);
+    }
+
+    private static function getLogFile($log_file_name = '')
+    {
+        if (!$log_file_name) $log_file_name = 'default';
+        $log_file = '/logs/' . $log_file_name . '.log-' . date('Y-m-d');
+        if (function_exists('storage_path')) {
+            $log_file = storage_path() . $log_file;
+        } else {
+            $log_file = '.' . $log_file;
+        }
+        return $log_file;
     }
 
     /**
